3a32a87bcdee0fcd15894f0a4a448bdceb359377
Georgiavans <georgiarayvanschie@gmail.com>
2022-11-10 11:45:03 -0400

    unpdated password length and character criteria promt

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-var userinput = window.prompt("How many characters would you like your password to have?")
+var userInput = window.prompt("How many characters would you like your password to have?")
+var passwordLength = parseInt("")
+
+if (isNaN(passwordLength)) {
+window.alert("that character is not a number")
+return
+}
+
+if (passwordLength < 8 || passwordLength > 128) {
+ window.alert("password length must be greater than 8 characters and no larger than 128 characters.") 
+ return
+}
 
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
