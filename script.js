e93d83783ba81721463c751581fb33ede320b1df
Georgiavans <georgiarayvanschie@gmail.com>
2022-11-10 11:22:24 -0400

    promt for password criteria

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+var userinput = window.prompt("How many characters would you like your password to have?")
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,4 +17,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-//text push
+ 
