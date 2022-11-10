5bd5aca9c8be9ce2606c8031b7f7a3c14bc6130f
Georgiavans <georgiarayvanschie@gmail.com>
2022-11-10 12:35:21 -0400

    lowercase, uppercase, numeric and symbols added options

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
 var userInput = window.prompt("How many characters would you like your password to have?")
-var passwordLength = parseInt("")
+var passwordLength = parseInt(userInput)
 
 if (isNaN(passwordLength)) {
 window.alert("that character is not a number")
@@ -16,6 +16,34 @@ if (passwordLength < 8 || passwordLength > 128) {
  return
 }
 
+var userWantsNumbers = window.confirm("Do you want to include numbers in your password?")
+var userWantsSymbols = window.confirm("Do you want to include symbols in your password?")
+var userWantsLowercase = window.confirm("Do you want to include lowercase letters in your password?")
+var userWantsUppercase = window.confirm("Do you want to include uppercase letters in your password?")
+
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7","8", "9"]
+var symbolList = [ "!", "@", "#", "$", "%", "^", "&", "*"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+ 
+var optionsAvailable = [numberList]
+
+if (userWantsNumbers === true) {
+optionsAvailable.push(numberList)
+}
+
+if (userWantsSymbols === true){
+optionsAvailable.push(symbolList)
+}
+
+if (userWantsLowercase === true){
+optionsAvailable.push(lowercaseList)
+}
+
+if (userWantsUppercase === true) {
+optionsAvailable.push(uppercaseList)
+}
+  
 }
 
 // Write password to the #password input
