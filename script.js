0111eca4618d0e30b3d68f3c75787c16c0c3931d
Georgiavans <georgiarayvanschie@gmail.com>
2022-11-12 16:52:39 -0400

    fix bugs

--- script.js
+++ script.js
@@ -2,23 +2,33 @@
 var generateBtn = document.querySelector("#generate");
 
 function randomInt(min, max) {
-return Math.floor(Math.random)*(max - min) + min
+  if (!max) {
+    max = min 
+    min = 0
+  }
+
+var rand = Math.random()
+  return Math.floor(min*(1 - rand) + (rand*max))
 }
 
-function generatePassword() {
-
-var userInput = window.prompt("How many characters would you like your password to have?")
-var passwordLength = parseInt(userInput)
-
-if (isNaN(passwordLength)) {
-window.alert("that character is not a number")
-return
+function getRandomItem(list) {
+  return list[randomInt(0, list.length -1)]
 }
 
-if (passwordLength < 8 || passwordLength > 128) {
- window.alert("password length must be greater than 8 characters and no larger than 128 characters.") 
- return
-}
+function generatePassword(thisIsPasswordLength) {
+
+  var userInput = window.prompt("How many characters would you like your password to have?")
+  var passwordLength = parseInt(userInput);
+
+  if (isNaN(passwordLength)) {
+    window.alert("that character is not a number")
+    return
+  };
+
+  if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("password length must be greater than 8 characters and no larger than 128 characters.") 
+  return
+  };
 
 var userWantsNumbers = window.confirm("Do you want to include numbers in your password?")
 var userWantsSymbols = window.confirm("Do you want to include symbols in your password?")
@@ -30,7 +40,8 @@ var symbolList = [ "!", "@", "#", "$", "%", "^", "&", "*"]
 var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
  
-var optionsAvailable = [numberList]
+
+var optionsAvailable = []
 
 if (userWantsNumbers === true) {
 optionsAvailable.push(numberList)
@@ -48,12 +59,15 @@ if (userWantsUppercase === true) {
 optionsAvailable.push(uppercaseList)
 }
 
+
 var generatedPassword = ""
 
-for (var i = 0; i < passwordLength; 1++) {
-  Math.random
+  for (var i = 0; i < passwordLength; i++) {
+    var randomList = getRandomItem(optionsAvailable)
+    var randomchar = getRandomItem(randomList)
+    generatedPassword += randomchar
 }
-
+return generatedPassword
 }
 
 // Write password to the #password input
